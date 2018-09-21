d0280e4af22a52432676e3e87809cd9bd213eae4
Yuya Minami <yuya.minami@rebase.co.jp>
2018-09-21 18:18:48 +0900

    ping pong

---
+++ src/api/mod.rs
@@ -0,0 +1,30 @@
+extern crate reqwest;
+use reqwest::{Client, Response, Result};
+
+#[derive(Debug, Deserialize)]
+pub struct RtmConnectResponse {
+    ok: bool,
+    pub url: String,
+    team: Team,
+    #[serde(rename = "self")]
+    me: Me,
+}
+#[derive(Debug, Deserialize)]
+struct Team {
+    id: String,
+    name: String,
+    domain: String,
+}
+#[derive(Debug, Deserialize)]
+struct Me {
+    id: String,
+    name: String,
+}
+
+pub fn rtm_connect(token: &str, client: &Client) -> Result<Response> {
+    let url = "https://slack.com/api/rtm.connect";
+    let builder = client.get(url);
+    let query = [("mpim_aware", "1")];
+
+    builder.bearer_auth(token).query(&query).send()
+}
---
+++ src/rtm/mod.rs
@@ -0,0 +1,68 @@
+extern crate ws;
+use api::RtmConnectResponse;
+use ws::util::Token;
+use ws::{CloseCode, Error, ErrorKind, Handler, Handshake, Message, Result, Sender};
+
+struct Client {
+    out: Sender,
+    message_id: u64,
+}
+
+impl Client {
+    fn new(out: Sender) -> Client {
+        Client { out, message_id: 0 }
+    }
+
+    fn send(&mut self, message: Message) -> Result<()> {
+        self.message_id += 1;
+        println!("← Outgoing:    {:?}", message);
+        self.out.send(message)
+    }
+
+    fn ping(&mut self) -> Result<()> {
+        let id = self.message_id;
+        let ping = format!("{{\"id\": \"{id}\", \"type\": \"ping\"}}", id = id);
+        self.send(ping.into())
+    }
+}
+
+const PING: Token = Token(0);
+impl Handler for Client {
+    fn on_open(&mut self, shake: Handshake) -> Result<()> {
+        println!("Handshake: {:?}", shake);
+        println!("Connection Opened");
+        self.out.timeout(5000, PING)?;
+        Ok(())
+    }
+
+    fn on_message(&mut self, message: Message) -> Result<()> {
+        println!("→ Incoming:    {:?}", message);
+        Ok(())
+    }
+
+    fn on_close(&mut self, code: CloseCode, reason: &str) {
+        match code {
+            CloseCode::Normal => println!("The server is done with the connection."),
+            CloseCode::Away => println!("The server is leaving"),
+            _ => println!("The server encountered an error: {}", reason),
+        }
+    }
+
+    fn on_error(&mut self, err: Error) {
+        println!("Error in rtm: {}", err);
+    }
+
+    fn on_timeout(&mut self, event: Token) -> Result<()> {
+        match event {
+            PING => {
+                self.ping()?;
+                self.out.timeout(5000, PING)
+            }
+            _ => Err(Error::new(ErrorKind::Internal, "Invalid timeout token")),
+        }
+    }
+}
+
+pub fn connect(response: &RtmConnectResponse) {
+    ws::connect(response.url.clone(), |out| Client::new(out)).unwrap();
+}
